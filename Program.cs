784e93e919f64ec0ed9a8c9859548cd7bd468462
nikolaykyrylishyn <nikolay.kyrylishyn@gmail.com>
2024-04-09 20:26:30 +0300

    change text

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Привіт, я навчаюся у школі Hillel!");
+            Console.ReadKey();
+        }
+    }
+}
