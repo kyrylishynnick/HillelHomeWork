65c1bc9102dab86e9106d519c396be8fd51b10ce
nikolaykyrylishyn <nikolay.kyrylishyn@gmail.com>
2024-04-12 13:38:22 +0300

    add tasks 4,5 and folder for task 6

--- Lesson3/HW_5/Program.cs
+++ Lesson3/HW_5/Program.cs
@@ -1,1 +1,6 @@
 ﻿//Користувач вводить радіус круга. Напишіть програму, яка обчислює площу цього круга і виводить в консоль результат. 
+
+Console.WriteLine("Inpur please a radius:");
+int radius = int.Parse(Console.ReadLine());
+
+Console.WriteLine("The area of a circle  = " + Math.PI * Math.Pow(radius, 2));
