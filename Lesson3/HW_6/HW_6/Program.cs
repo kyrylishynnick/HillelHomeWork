788f6f7f9bc45ef806a6e36af9c188529067143e
nikolaykyrylishyn <nikolay.kyrylishyn@gmail.com>
2024-04-12 15:44:57 +0300

    add task 6

--- Lesson3/HW_6/HW_6/Program.cs
+++ Lesson3/HW_6/HW_6/Program.cs
@@ -1,1 +1,17 @@
 ﻿// Дослідити інші способи поміняти місцями значення двох змінних без використання тимчасової змінної та використання суми/множення
+
+Console.WriteLine("Hello, please enter the first number");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine($"The first number before changing is {firstNumber}");
+Console.WriteLine("Hello, please enter the second number");
+int secondNumber = int.Parse(Console.ReadLine());
+Console.WriteLine($"The second number before changing is {secondNumber}");
+
+
+firstNumber = firstNumber ^ secondNumber;
+secondNumber = firstNumber ^ secondNumber;
+firstNumber = firstNumber ^ secondNumber;
+
+Console.WriteLine("________________________________________________");
+Console.WriteLine($"The first number after changing: {firstNumber}");
+Console.WriteLine($"The first number after changing: {secondNumber}");
