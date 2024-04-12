8edf635bb37ab6979d99d7f6d65d0d518d5dd701
nikolaykyrylishyn <nikolay.kyrylishyn@gmail.com>
2024-04-12 13:25:28 +0300

    add task 3

--- Lesson3/HW_3/Program.cs
+++ Lesson3/HW_3/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Дано тризначне число. Знайти число, отримане під час перестановки першої та другої цифр заданого числа.
 
-Console.WriteLine("Hello, World!");
+int number = 246;
+
+int firstTemporaryDig;
+int secondTemporaryDig;
+int thirtdTemporaryDig;
+
+
+firstTemporaryDig = number / 100;
+secondTemporaryDig = number / 10 % 10;
+thirtdTemporaryDig = number % 10;
+
+int revercedNumber = secondTemporaryDig * 100 + firstTemporaryDig * 10 + thirtdTemporaryDig;
+
+Console.WriteLine($"The reverce number is: {revercedNumber}");
--- Lesson3/HW_4/Program.cs
+++ Lesson3/HW_4/Program.cs
@@ -1,3 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿// Написати програму, яка обчислює середнє арифметичне двох чисел.
