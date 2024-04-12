e86cb67c1a2f25775bbbbd0d8b7121031fbd4c19
nikolaykyrylishyn <nikolay.kyrylishyn@gmail.com>
2024-04-12 12:42:49 +0300

    add task 2

--- Lesson3/HW_2/Program.cs
+++ Lesson3/HW_2/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Написати програму, яка обчислює квадрат будь-якого введеного числа (бажано використати Math).
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Inpur please a number:");
+double userInput = double.Parse(Console.ReadLine());
+
+double square = Math.Pow(userInput, 2);
+Console.WriteLine($"The square of the number {userInput} is {square}");
