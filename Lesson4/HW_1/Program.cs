92678fef337e619c148d57c40f0483439041ee36
nikolaykyrylishyn <nikolay.kyrylishyn@gmail.com>
2024-04-15 20:02:21 +0300

    add hw tasks 1,2

--- Lesson4/HW_1/Program.cs
+++ Lesson4/HW_1/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Необхідно написати консольну програму, де користувач вводитиме з клавіатури 2 числа.
+//Числа будуть порівнюватися з наступним виведенням в консоль результату цього порівняння (чи рівні значення,
+//а якщо ні, яке число більше/менше).
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, please input the first number");
+int firstUserInput = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Hello, please input the second number");
+int secondUserInput = int.Parse(Console.ReadLine());
+Console.WriteLine($"The first number is: {firstUserInput}. The second  number is: {secondUserInput}");
+
+if (firstUserInput > secondUserInput)
+{
+    Console.WriteLine("The first value is bigger than second");
+}
+else if (firstUserInput < secondUserInput)
+{
+    Console.WriteLine("The second value is bigger than first");
+}
+else if (firstUserInput == secondUserInput)
+{
+    Console.WriteLine("The numbers are equal");
+}
--- Lesson4/HW_2/Program.cs
+++ Lesson4/HW_2/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Написати програму, в якій користувач вводить номер місяця.
+// Якщо місяць 1,2,12 відображає "Зима"; 3,4,5 - «Весна»; 6-8 - «Літо»; 9-11 – «Осінь».
+// У будь-якому іншому випадку - "Немає такого місяця на цій планеті" .
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, please enter the number of month to detect the season");
+int userInput = int.Parse(Console.ReadLine());
+
+if (userInput == 1 || userInput == 2 || userInput == 12)
+{
+    Console.WriteLine("So, today is winter");
+}
+else if (userInput == 3 || userInput == 4 || userInput == 5)
+{
+    Console.WriteLine("So, today is autumn");
+}
+else if (userInput == 6 || userInput == 7 || userInput == 8)
+{
+    Console.WriteLine("So, today is summer");
+}
+else if (userInput == 9 || userInput == 10 || userInput == 11)
+{
+    Console.WriteLine("So, today is spring");
+}
+else
+{
+    Console.WriteLine("Sorry, we don't have this month in the world!");
+}
