f9dc043971106dc2c729b794f044f9c8bdd9a230
nikolaykyrylishyn <nikolay.kyrylishyn@gmail.com>
2024-04-18 12:48:22 +0300

    add hw 3

--- Lesson5/HW_3/Program.cs
+++ Lesson5/HW_3/Program.cs
@@ -1,1 +1,25 @@
 ﻿//  Доробити таблицю множення - зробити гарний вивід в консоль.
+
+for (int i = 1; i <= 10; i++)
+
+{
+    Console.WriteLine();
+    for (int j = 1; j < 6; j++)
+
+    {
+        Console.Write($"{j} * {i} = {i * j}");
+        if (j != 5)
+            Console.Write("\t");
+    }
+}
+
+Console.WriteLine();
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine();
+    for (int j = 6; j <= 10; j++)
+    {
+        Console.Write($"{j} * {i} = {i * j}");
+        Console.Write("\t");
+    }
+}
