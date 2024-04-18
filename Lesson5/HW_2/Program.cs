63d1db13cb7a7a465094305aeda7e82538bc706e
nikolaykyrylishyn <nikolay.kyrylishyn@gmail.com>
2024-04-18 14:26:58 +0300

    add hm 2

--- Lesson5/HW_2/Program.cs
+++ Lesson5/HW_2/Program.cs
@@ -1,3 +1,21 @@
 ﻿// Програма загадує число від 1 до 146 (привіт, Random).
 // Користувач намагається вгадати його. У разі неправильної відповіді програма підказує «більше» або «менше»
 
+Random random = new Random();
+int randomNumber = random.Next(1, 146);
+Console.WriteLine(randomNumber);
+Console.WriteLine("Hello, try to guess the number were generated");
+
+while (true)
+{
+    int userGuess = int.Parse(Console.ReadLine());
+    if (randomNumber == userGuess)
+    {
+        Console.WriteLine("Hey. You are lucky. You choice is rigth!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Hmmmm. You made a mistake. Try again!");
+    }
+}
