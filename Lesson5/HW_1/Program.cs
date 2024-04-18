567498e5e8ff14714683a8459d1ffe93989bc590
nikolaykyrylishyn <nikolay.kyrylishyn@gmail.com>
2024-04-18 14:08:56 +0300

    add hw 1

--- Lesson5/HW_1/Program.cs
+++ Lesson5/HW_1/Program.cs
@@ -1,2 +1,18 @@
 ﻿// Напишіть програму, яка «запитуватиме» правильний пароль, доки він не буде введений.
 // Правильний пароль нехай буде "root". Якщо пароль неправильний, програма повинна сказати "Неправильний пароль!"
+
+
+string userPassword = "root";
+Console.WriteLine("Hello, please enter the password");
+do
+{
+    string userInput = Console.ReadLine();
+    if (userPassword == userInput)
+    {
+        Console.WriteLine("Awesome. You entered the correct password!");
+    }
+    else
+    {
+        Console.WriteLine("Ohhhh. Sorry, but you password is incorrect");
+    }
+} while (true);
