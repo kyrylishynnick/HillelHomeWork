4ff18284684ced9c130cbd823f1a229ac7b742c6
nikolaykyrylishyn <nikolay.kyrylishyn@gmail.com>
2024-04-22 21:24:44 +0300

    add hm 2,4

--- Lesson6/HW_2/Program.cs
+++ Lesson6/HW_2/Program.cs
@@ -1,4 +1,43 @@
-﻿// Написати програму для інверсії масиву, тобто перевороту його у зворотному порядку.
-// В цьому завданні недостатньо просто вивести елементи масиву у зворотному порядку.
-// Ідеально написати 2 способами (*вища оцінка буде) - через новий масив та переписавши значення заданного масиву.
+﻿//Написати програму для інверсії масиву, тобто перевороту його у зворотному порядку.
+//В цьому завданні недостатньо просто вивести елементи масиву у зворотному порядку. 
+// Ідеально написати 2 способами (*вища оцінка буде) - через новий масив та переписавши значення заданного масиву. 
 // {5, -9, 8, 7} => {7, 8, -9, 5}
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 5, -9, 8, 7 };
+
+
+        int[] reservedArray = new int[array.Length];
+        for (int i = 0; i < array.Length; i++)
+        {
+            reservedArray[i] = array[array.Length - 1 - i];
+        }
+
+
+        for (int i = 0; i < array.Length / 2; i++)
+        {
+            int newTempArray = array[i];
+            array[i] = array[array.Length - 1 - i];
+            array[array.Length - 1 - i] = newTempArray;
+        }
+
+        Console.WriteLine("The first variant:");
+        PrintArray(reservedArray);
+
+        Console.WriteLine("\nThe second variant:");
+        PrintArray(array);
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (var item in array)
+        {
+            Console.Write(item + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
--- Lesson6/HW_4/Program.cs
+++ Lesson6/HW_4/Program.cs
@@ -1,2 +1,23 @@
 ﻿// Написати програму, яка виводить всі елементи масиву доки не зустрінеться елемент -1.
 // Масив заповнити рандомними числами, діапазон чисел від -5 до 5. 
+
+int[] array = new int[30];
+
+Random rnd = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(-5, 6);
+}
+
+
+foreach (int num in array)
+{
+    if (num != -1)
+    {
+        Console.Write(num + " ");
+    }
+    else
+    {
+        break;
+    }
+}
