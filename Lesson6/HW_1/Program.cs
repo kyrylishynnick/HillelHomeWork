4054d059d4e6471c36689aa7c8ac83010cd59cd3
nikolaykyrylishyn <nikolay.kyrylishyn@gmail.com>
2024-04-22 13:25:54 +0300

    add HW6 task 1

---
+++ Lesson6/HW_1/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задати масив із 10 елементів. Заповнити цей масив рандомними числами від -100 до 100.
+//Знайти кількість додатніх чисел у масиві.
+//Вивести на екран масив і кількість порахованих чисел.
+//{5, -9, 8, 7} =>{5, -9, 8, 7} , кількість додатних чисел = 3  
+//(фігурні дужки і коми, звісно, можна не виводити в консоль!!!)
+
+
+int[] arrayElements = new int[10];
+Random random = new Random();
+
+for (int i = 0; i < arrayElements.Length; i++)
+{
+    arrayElements[i] = random.Next(-100, 100);
+}
+
+int count = 0;
+foreach (int number in arrayElements)
+{
+    if (number > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine("The array is:");
+foreach (int number in arrayElements)
+{
+    Console.Write(number + " ");
+}
+
+Console.WriteLine("\nThe number of positive symbols in the array: " + count);
